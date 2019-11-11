2c53888947ab2c2ffafa847a0b5a7b0cfd801137
MistyCoders <MistyCoders@gmail.com>
2019-11-12 00:08:23 +0900

    frontend

--- src/Routes/Profile/ProfileContainer.js
+++ src/Routes/Profile/ProfileContainer.js
@@ -14,6 +14,6 @@ export const LOG_OUT = gql`
 
 export default withRouter(({ match: { params: { username } } }) => {
     const { data, loading } = useQuery(GET_USER, { variables: { userName: username } });
-    const logOut = useMutation(LOG_OUT);
+    const [logOut] = useMutation(LOG_OUT);
     return <ProfilePresenter loading={loading} logOut={logOut} data={data} />;
 });
