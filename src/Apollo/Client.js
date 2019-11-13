7ffeaecbd9d88241ca71b1e30a1d527726ed9d74
mistycoders <mistycoders@gmail.com>
2019-11-14 02:36:07 +0900

    url

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,12 +2,13 @@ import AppolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new AppolloClient({
-    uri: "http://localhost:4000",
+    uri:
+        process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://mistygram-backend.herokuapp.com/",
     clientState: {
         defaults,
         resolvers
     },
     headers: {
-        "Authorization" : `Bearer ${localStorage.getItem("token")}`
+        "Authorization": `Bearer ${localStorage.getItem("token")}`
     }
 });
